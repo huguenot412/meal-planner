dacfdbcb3f3dab91d22dfd4d9cfb249f178cad0d
Chris Snow <chris.snow@treehousei.net>
2020-01-20 18:12:43 -0700

    Refactored MealPlanService

--- src/app/meal-plans/meal-plans.service.ts
+++ src/app/meal-plans/meal-plans.service.ts
@@ -10,38 +10,36 @@ import * as moment from 'moment';
 })
 export class MealPlansService {
 
-  mealPlans: MealPlan[] = [];
-
-  // mealPlanMap: MealPlanMap = {}
+  private mealPlans: MealPlan[] = [];
+  private mealPlanMap: MealPlanMap = {};
 
   constructor() { }
 
-  createMealPlanMap(): MealPlanMap {
-    const mealPlanMap: MealPlanMap = {};
-    this.mealPlans.forEach(mealPlan => mealPlanMap[mealPlan.id] = mealPlan);
-    return mealPlanMap;
+  createMealPlanMap(): void {
+    this.mealPlans.forEach(mealPlan => this.mealPlanMap[mealPlan.id] = mealPlan);
   }
 
   getMeals(): MealPlanMap {
-    return this.createMealPlanMap();
+    this.createMealPlanMap();
+    return this.mealPlanMap;
   }
 
   addMeal(mealPlan: MealPlan, newMeal: string): void {
-    const mealPlanMap: MealPlanMap = this.createMealPlanMap()
-    if(mealPlanMap[mealPlan.id]) {
-     mealPlanMap[mealPlan.id].meals.push(new Meal(newMeal));
+    this.createMealPlanMap();
+    if(this.mealPlanMap[mealPlan.id]) {
+     this.mealPlanMap[mealPlan.id].meals.push(new Meal(newMeal));
     } else {
       this.mealPlans.push(new MealPlan(mealPlan.date, [new Meal(newMeal)]));
     }  
   }
 
   deleteMeal(mealPlanId: string, mealId: number): void {
-    const mealPlanMap: MealPlanMap = this.createMealPlanMap();
-    mealPlanMap[mealPlanId].meals = mealPlanMap[mealPlanId].meals.filter(meal => meal.id !== mealId);
+    this.createMealPlanMap();
+    this.mealPlanMap[mealPlanId].meals = this.mealPlanMap[mealPlanId].meals.filter(meal => meal.id !== mealId);
   }
 
   editMeal(mealPlanId: string, mealId: number, newValue: string): void {
-    const mealPlanMap: MealPlanMap = this.createMealPlanMap();
-    mealPlanMap[mealPlanId].meals.find(meal => meal.id === mealId).name = newValue;
+    this.createMealPlanMap();
+    this.mealPlanMap[mealPlanId].meals.find(meal => meal.id === mealId).name = newValue;
   }
 }
